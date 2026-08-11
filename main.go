55cf16bbcac76fc2547933c49f1cfc22ffc580f5
Lando <lando_vargas@outlook.com>
2026-08-11 00:14:47 -0400

    CH1: L3 instructor given solution

--- main.go
+++ main.go
@@ -1,41 +1,40 @@
 package main
 
 import (
-	"fmt"
-	"landovargas/blog-aggregator/internal"
-	"landovargas/blog-aggregator/internal/config"
 	"log"
 	"os"
+
+	"landovargas/blog-aggregator/internal/config"
 )
+
+type state struct {
+	cfg *config.Config
+}
 
 func main() {
 	cfg, err := config.Read()
 	if err != nil {
 		log.Fatalf("error reading config: %v", err)
 	}
-	fmt.Printf("Read config: %+v\n", cfg)
 
-	var state config.State
-	state.Config = &cfg
+	programState := &state{
+		cfg: &cfg,
+	}
 
-	var commands config.Commands
-	commands.Commands = make(map[string]func(*config.State, config.Command) error)
-	commands.Register("login", internal.HandlerLogins)
+	cmds := commands{
+		registeredCommands: make(map[string]func(*state, command) error),
+	}
+	cmds.register("login", handlerLogin)
 
 	if len(os.Args) < 2 {
-		fmt.Println("No arguments were given!")
-		os.Exit(1)
+		log.Fatal("Usage: cli <command> [args...]")
 	}
 
-	var cmd config.Command
-	cmd.Name = os.Args[1]
-	cmd.Arguments = os.Args[2:]
+	cmdName := os.Args[1]
+	cmdArgs := os.Args[2:]
 
-	err = commands.Run(&state, cmd)
+	err = cmds.run(programState, command{Name: cmdName, Args: cmdArgs})
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		log.Fatal(err)
 	}
-
-	os.Exit(0)
 }
