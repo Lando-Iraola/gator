723db7ba9a71c46e42df1666e77450de1642b59b
Lando <lando_vargas@outlook.com>
2026-08-12 01:48:40 -0400

    CH3: L2 instructor given solution

--- handler_feed.go
+++ handler_feed.go
@@ -9,33 +9,44 @@ import (
 	"github.com/google/uuid"
 )
 
-func handlerFeed(s *state, cmd command) error {
+func handlerAddFeed(s *state, cmd command) error {
+	user, err := s.db.GetUser(context.Background(), s.cfg.CurrentUserName)
+	if err != nil {
+		return err
+	}
+
 	if len(cmd.Args) != 2 {
-		return fmt.Errorf("usage: %v <name> <url>", cmd.Name)
+		return fmt.Errorf("usage: %s <name> <url>", cmd.Name)
 	}
 
 	name := cmd.Args[0]
 	url := cmd.Args[1]
 
-	user, err := s.db.GetUser(context.Background(), s.cfg.CurrentUserName)
+	feed, err := s.db.CreateFeed(context.Background(), database.CreateFeedParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		UserID:    user.ID,
+		Name:      name,
+		Url:       url,
+	})
 	if err != nil {
-		return fmt.Errorf("Failed to find user: %v", err)
+		return fmt.Errorf("couldn't create feed: %w", err)
 	}
 
-	var feed database.CreateFeedParams
-
-	feed.ID = uuid.New()
-	feed.CreatedAt = time.Now()
-	feed.UpdatedAt = time.Now()
-	feed.Name = name
-	feed.Url = url
-	feed.UserID = user.ID
-	newFeed, err := s.db.CreateFeed(context.Background(), feed)
-	if err != nil {
-		return fmt.Errorf("Failed to create new feed: %w", err)
-	}
-
-	fmt.Printf("%v", newFeed)
+	fmt.Println("Feed created successfully:")
+	printFeed(feed)
+	fmt.Println()
+	fmt.Println("=====================================")
 
 	return nil
+}
+
+func printFeed(feed database.Feed) {
+	fmt.Printf("* ID:            %s\n", feed.ID)
+	fmt.Printf("* Created:       %v\n", feed.CreatedAt)
+	fmt.Printf("* Updated:       %v\n", feed.UpdatedAt)
+	fmt.Printf("* Name:          %s\n", feed.Name)
+	fmt.Printf("* URL:           %s\n", feed.Url)
+	fmt.Printf("* UserID:        %s\n", feed.UserID)
 }
